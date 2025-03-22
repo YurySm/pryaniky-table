43aeb569c3d81a2f031f9fb13b64eaf295b96ec0
y.smolka <yurysmolka@gmail.com>
2025-03-22 20:56:14 +0300

    lint

--- src/pages/MainPage/ui/MainPage.stories.ts
+++ src/pages/MainPage/ui/MainPage.stories.ts
@@ -1,16 +1,30 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import MainPage from './MainPage';
 import { StoreDecorator } from 'shared/config/storybook/decorators/StoreDecorator';
+import { DocsResponseItem } from 'entities/Docs';
+
+const docs: DocsResponseItem[] = [
+    {
+        id: '1',
+        documentName: 'test',
+        companySigDate: '2025-03-22T12:16:44.545Z',
+        companySignatureName: 'test',
+        documentStatus: 'test',
+        documentType: 'test',
+        employeeNumber: 'test',
+        employeeSigDate: '2025-03-22T12:16:44.545Z',
+        employeeSignatureName: 'test',
+    }
+]
 
 const meta: Meta<typeof MainPage> = {
     title: 'pages/MainPage',
     component: MainPage,
     decorators: [StoreDecorator({
-        user: {
-            authData: 'test'
-        },
         docs: {
-            docsItems: []
+            isLoading: false,
+            error: undefined,
+            docsItems: docs
         }
     })]
 };
